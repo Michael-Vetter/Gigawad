645100be69bef0ecf08d13d2941360ae8749c083
Michael Vetter <michaelvetter@yahoo.com>
2017-11-02 13:07:15 -0400

    add blog with nosql

---
+++ Gigawad/wwwroot/Scripts/LambdaBlogEntries.js
@@ -0,0 +1,24 @@
+﻿(function () {
+
+    var getLambdaBlogEntries = function ($http, $log) {
+
+        $log.info("getLambdaBlogEntries GH1"); 
+        var getStaticBlogEntries = function () {
+            return $http.get("https://y7sr1cwrua.execute-api.us-west-2.amazonaws.com/Prod/")
+                .then(function (response) {
+                    $log.info("getLambdaBlogEntries GH1a"); 
+                    return response.data;
+                });
+        };
+        $log.info("getLambdaBlogEntries GH2"); 
+        return {
+            getStaticBlogEntries: getStaticBlogEntries
+        };
+        
+    }
+
+    var module = angular.module("app");
+
+    module.factory("getLambdaBlogEntries", getLambdaBlogEntries);
+
+}());
